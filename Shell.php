bda6736d9b5e9ae59d4a9ce8f8d9be295e538016
Edrard A <demonand@gmail.com>
2018-07-05 15:02:04 +0300

    Shell run command function

--- Shell.php
+++ Shell.php
@@ -4,3 +4,6 @@ function command_exist($cmd) {
     $returnVal = trim(shell_exec("type $cmd"));
     return (empty($returnVal) ? false : true);
 }
+function run_shell_command($command, $param){
+    exec($command.' '.$param);       
+}
