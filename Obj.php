9fcd5023702088b608e06ed1f42bdb9712b4ab72
Edrard A <demonand@gmail.com>
2018-08-29 15:00:23 +0300

    Adding Object functions

---
+++ Obj.php
@@ -0,0 +1,13 @@
+<?php
+
+if (! function_exists('obj_to_array')) {   
+    /**
+    * Converting Object to array
+    * 
+    * @param stdClass $obj
+    * @return array
+    */
+    function obj_to_array($obj){
+        return json_decode(json_encode($obj),TRUE);    
+    }
+}
