0fd69053ea27da35d033921e83bce0a51e184418
Edrard A <demonand@gmail.com>
2024-02-01 15:08:31 +0200

    fix: remove space

--- Cli.php
+++ Cli.php
@@ -1,6 +1,6 @@
 <?php
 if (! function_exists('cli_confirm')) {
-    function cli_confirm ($text = "Are you sure you want to do this?  Type 'yes' to continue: \n",$thanks = "\nThank you, continuing...\n",$error = "Exiting...\n") {
+    function cli_confirm($text = "Are you sure you want to do this?  Type 'yes' to continue: \n",$thanks = "\nThank you, continuing...\n",$error = "Exiting...\n") {
         echo $text;
         $line = fgets(fopen ("php://stdin","r"));
         preg_match('/((yes)|y)$/i', $line, $output_array);
