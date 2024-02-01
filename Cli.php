fe5e25b0aadac02ec9c258eeee18139ff80398d6
Edrard A <demonand@gmail.com>
2024-02-01 15:20:41 +0200

    fix: bug with handle

--- Cli.php
+++ Cli.php
@@ -2,7 +2,8 @@
 if (! function_exists('cli_confirm')) {
     function cli_confirm($text = "Are you sure you want to do this?  Type 'yes' to continue: \n",$thanks = "\nThank you, continuing...\n",$error = "Exiting...\n") {
         echo $text;
-        $line = fgets(fopen ("php://stdin","r"));
+        $handle = fopen ("php://stdin","r");
+        $line = fgets($handle);
         preg_match('/((yes)|y)$/i', $line, $output_array);
         if($output_array == array()){
             echo $error;
